e74ffc65d3a1d57e5d2f6e7a1a4dd094aedc8919
ronyislam502 <ronyislam502@gmail.com>
2025-04-07 18:48:58 +0600

    custome textarea

---
+++ src/components/form/ETextarea.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useFormContext, useWatch } from "react-hook-form";
+
+import { IInput } from "@/src/types";
+import { Textarea } from "@heroui/input";
+
+interface IProps extends IInput {
+  type?: string;
+}
+
+const ETextarea = ({ name, label, variant = "bordered" }: IProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const currentValue = useWatch({ name });
+
+  return (
+    <Textarea
+      {...register(name)}
+      label={label}
+      minRows={6}
+      value={currentValue || ""}
+      variant={variant}
+    />
+  );
+};
+
+export default ETextarea;
